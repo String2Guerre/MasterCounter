52df63b8eb38c964db8cfc9253a9661ef78747ba
Davon <tie-n-dev@ntymail.com>
2018-08-09 16:43:29 +0200

    Modify skill and user model to implement MtoM relashionship

--- MasterCounter/MasterCounter.WebApp/Models/Skill.cs
+++ MasterCounter/MasterCounter.WebApp/Models/Skill.cs
@@ -10,5 +10,6 @@ namespace MasterCounter.WebApp.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public ICollection<User> Users { get; set; }
     }
 }
--- MasterCounter/MasterCounter.WebApp/Models/User.cs
+++ MasterCounter/MasterCounter.WebApp/Models/User.cs
@@ -10,5 +10,7 @@ namespace MasterCounter.WebApp.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public ICollection<Skill> Skills { get; set; }
+
     }
 }
