94ddfc0185faa3431977e45cd9bb124036a4e924
Davon <tie-n-dev@ntymail.com>
2018-08-09 17:12:16 +0200

    Add IDal interface

---
+++ MasterCounter/MasterCounter.WebApp/DAL/IDal.cs
@@ -0,0 +1,14 @@
+﻿using MasterCounter.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterCounter.WebApp.DAL
+{
+    public interface IDal : IDisposable
+    {
+        List<Skill> GetAllSkills();
+    }
+}
