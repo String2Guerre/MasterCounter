329900c2199384d79098b62053ca4d8122b8fca3
Davon <tie-n-dev@ntymail.com>
2018-08-09 16:50:24 +0200

    Add DbContext class

---
+++ MasterCounter/MasterCounter.WebApp/Models/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MasterCounter.WebApp.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Skill> Skills { get; set; }
+
+    }
+}
