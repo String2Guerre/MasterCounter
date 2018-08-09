1136d102ec3c95d5207043c8de7e43e05449e29f
Davon <tie-n-dev@ntymail.com>
2018-08-09 16:43:00 +0200

    Add developping model

---
+++ MasterCounter/MasterCounter.WebApp/Models/Developping.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MasterCounter.WebApp.Models
+{
+    public class Developping
+    {
+        public virtual User User { get; set; }
+        public virtual Skill Skill { get; set; }
+        public double HoursPassed { get; set; }
+    }
+}
