3e5464e847f69b45be96a19ec2a1e4cd6d01396e
Davon <tie-n-dev@ntymail.com>
2018-08-09 17:10:41 +0200

    Move DbContext into DAL folder

--- MasterCounter/MasterCounter.WebApp/DAL/DatabaseContext.cs
+++ MasterCounter/MasterCounter.WebApp/DAL/DatabaseContext.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using MasterCounter.WebApp.Models;
 
-namespace MasterCounter.WebApp.Models
+namespace MasterCounter.WebApp.DAL
 {
     public class DatabaseContext : DbContext
     {
