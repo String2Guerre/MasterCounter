6165b277e8fe8fab32c14a922a626e814c708192
Davon <tie-n-dev@ntymail.com>
2018-08-09 17:15:25 +0200

    Add Dal class

---
+++ MasterCounter/MasterCounter.WebApp/DAL/Dal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MasterCounter.WebApp.Models;
+
+namespace MasterCounter.WebApp.DAL
+{
+    public class Dal : IDal
+    {
+        private DatabaseContext DB;
+
+        public Dal()
+        {
+            DB = new DatabaseContext();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Skill> GetAllSkills()
+        {
+            return DB.Skills.ToList();
+        }
+    }
+}
